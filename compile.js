9d3eda092753a1317acbe49237c23297b94367a9
Francesc Bagué Martí <francesc.bague@gmail.com>
2023-06-08 19:44:20 +0200

    Mejorada la funcion de obtencion de archivos para la compresion del sass

--- compile.js
+++ compile.js
@@ -1,26 +1,42 @@
 const { exec, execSync } = require("child_process");
 const path = require('path')
 const fs = require('fs')
+const args = process.argv.slice(2)
+const isDir = (route) => fs.statSync(route).isDirectory()
 
-const files_js = fs.readdirSync('src/sass')
-let sass_command = 'sass -s compressed'
+function search_files(route, arr=[]) {
+    fs.readdirSync(route).map((value) => {
+        const new_route = path.join(route, value);
+        if ( isDir(new_route) ) {
+            return search_files(new_route, arr)
+        } else {
+            arr.push(new_route)
+        }
+    })
+    return arr
+}
+const files_sass = search_files('src/sass')
+let sass_command = 'npx sass --no-source-map -s compressed'
 
-files_js.map(file => {
-    const input = `src/sass/${file}`
-    const output = `dist/css/${file.replace('.scss','.min.css')}`
-    
+files_sass.map(pth => {
+    const input = pth
+    const output = pth.replace('src/sass/','dist/css/').replace('.scss','.min.css')
     sass_command = `${sass_command} ${input}:${output}`;
-
-    
-    // console.log(command)
 })
 
-
-function execute_command(command, str=false) {
-    if (str) {
-        console.log(`\n${str}`);
+function execute_command(command, log=undefined, output=undefined) {
+    if (log) {
+        if (!output) { output = command } 
+        console.log('>',output);
     }
-    console.log(execSync(command).toString())
+    execSync(command)
 }
-execute_command('npm run build')
-execute_command(sass_command)
+
+console.log('\n')
+console.log('$ npm run build')
+console.log(execSync('npm run build').toString().replace('\n\n','\n'));
+
+if (args.includes('--minify')) {
+    execute_command('uglifyjs-folder dist/js -eo dist/js', true)
+    execute_command(sass_command, true, 'npx sass --no-source-map -s compressed sec/sass:dist/css')
+}
